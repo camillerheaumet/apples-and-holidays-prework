0c139ffe1030516be1debe1b26356b1063f5cf36
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-24 19:39:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def all_supplies_in_holidays(holiday_hash)
       
       supply.join(", ").to_s
      
-      #holiday_hash = season.to_s, holiday.to_s.capitalize!, supply.to_s
+      #holiday_hash = season.to_s, holiday.to_s.capitalize!, [supply.to_s]
        binding.pry
     end
    # binding.pry
